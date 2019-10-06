b8b71255af20715dd2a4ff125cf778a51a2f8f93
DESKTOP-GSFOR05\augus <a.tankam@synclab.it>
2019-10-06 17:24:21 +0200

    Add logger

--- src/main/java/it/synclab/stage/cogeweb/Util/HibernateUtil.java
+++ src/main/java/it/synclab/stage/cogeweb/Util/HibernateUtil.java
@@ -2,10 +2,12 @@ package it.synclab.stage.cogeweb.Util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
-	 
+	 Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
